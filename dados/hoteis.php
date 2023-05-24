b39ef6b343276cddb59cf313521403883522fd30
Jamylle Campos <jamyllecampos2501@gmail.com>
2023-05-23 22:23:28 -0300

    dados dos hotéis

---
+++ dados/hoteis.php
@@ -0,0 +1,68 @@
+<?php
+$hoteis =[
+    /*Santa Catarina */
+    [
+        "nome" => "Hotel Fazenda Park",
+        "local" => "Florianópolis - Santa Catarina",
+        "avaliacao" => "9.5 - Excelente",
+        "preco" => "R$240,00 pernoite."
+    ],
+
+    [
+        "nome" => "Hotel Isabela Duncan",
+        "local" => "Criciúma - Santa Catarina",
+        "avaliacao" => "8.3 - Muito bom",
+        "preco" => "R$103,00 pernoite."
+    ],
+
+    [
+        "nome" => "Hotel Monsieur",
+        "local" => "Gravatal - Santa Catarina",
+        "avaliacao" => "9.1 - Muito bom",
+        "preco" => "R$112,00 pernoite."
+    ],
+    /* Rio Grande Do Sul */
+    [
+        "nome" => "Hotel Kitnet Rififi",
+        "local" => "Caxias do Sul - Rio Grande Do Sul",
+        "avaliacao" => "8,6 - Muito bom",
+        "preco" => "R$60,00 pernoite."
+    ],
+
+    [
+        "nome" => "Hotel Petrov",
+        "local" => "Porto Alegre - Rio Grande Do Sul",
+        "avaliacao" => "7.8 - Bom",
+        "preco" => "R$59,00 pernoite." 
+    ],
+
+      [
+        "nome" => "Hotel Finn Kaster Silva",
+        "local" => "Três Coroas - Rio Grande Do Sul",
+        "avaliacao" => "9.5 - Muito bom",
+        "preco" => "R$114,00 pernoite."
+        
+      ],
+      /* Paraná */
+      [
+        "nome" => "Hotel Evelina",
+        "local" => "Curitiba - Paraná",
+        "avaliacao" => "9.9 - Excelente",
+        "preco" => "R$280,00 pernoite."
+      ],
+
+      [
+        "nome" => "Hotel Triunfo",
+        "local" => "Londrina - Paraná",
+        "avaliacao" => "8.1 - Bom",
+        "preco" => "R$63,00 pernoite."
+      ],
+
+      [
+        "nome" => "Hotel Tarobá",
+        "local" => "Foz do Iguaçu - Paraná",
+        "avaliacao" => "9.8 - Excelente",
+        "preco" => "R$257,00 pernoite."
+      ]
+
+    ];
