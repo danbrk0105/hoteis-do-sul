839138f79f04879d5ebcf7b09c17d07e14cec1ba
Danilo Cerqueira <114227989+danbrk0105@users.noreply.github.com>
2023-05-25 16:20:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 <html>
     <head>
     <title>Hotéis do Sul</title>
-    <link rel="stylesheet" type="text/css" href="estilo/estilo.css">
+    <link rel="stylesheet" type="text/css" href="estilo/estilos.css">
     </head>
     <body>
         <header>
-            <img src="imagens/logo.png" alt="">
+            <img src="imagens/logo2.png" alt="">
             <nav>
             <ul>
                 <li><a href="index.php">Início</a></li>
@@ -22,52 +22,28 @@
 
         <section id="banner">
             <h2>Bem-vindos ao Hotéis do Sul</h2>
-            <p>Escolha o estado desejado e aproveite nossos preços.</p>
-            <a href="teste1.php" class="button">Clique aqui para ver os quartos</a>
+            <p>Escolha o hotel desejado e aproveite nossos preços.</p>
+            <a href="paginas/listagemhoteis.php" class="button">Clique aqui para ver os hoteis</a>
         </section>
 
     <section id="rooms">
-        <h2>Quartos mais acessados</h2>
+        <h2>Hoteis mais acessados</h2>
         <div class="room">
-        <img src="imagens/img1.jpg" alt="Room 1">
-        <h3>Recanto Cataratas - Thermas Resort & Convention</h3>
+        <img src="imagens/HotelKitnetRififi.jpg" alt="Room 1">
+        <h3>Hotel Kitnet Rififi</h3>
         <p>Hotel muito bom. Limpo e com instalacoes bonitas. Bom atendimento.</p>
-        <a href="#" class="button">Mais informações</a>
         </div>
         <div class="room">
         <img src="imagens/img2.jpg" alt="Room 2">
         <h3>Hotel Fazzenda Park</h3>
         <p>Excelente, ótimo atendimento, ambientes confortáveis, limpos e aconchegante.</p>
-        <a href="#" class="button">Mais informações</a>
         </div>
         <div class="room">
-        <img src="imagens/img5.jpg" alt="Room 3">
-        <h3>Daara</h3>
+        <img src="imagens/HotelTriunfo.jpg" alt="Room 3">
+        <h3>Hotel Triunfo</h3>
         <p>Educação dos funcionarios, agilidade no atendimento, quarto luxuoso.</p>
-        <a href="#" class="button">Mais informações</a>
         </div>
     </section>
-
-    <!--<div class="container">
-            <?php
-                foreach($hoteis as $i => $h){
-
-            ?>
-            <div class="card-hotel">
-                <a href="paginas/listagemhoteis.php?i=<?=$i?>">
-                    <img src="<?=$h["foto"]?>" alt="<?= $h["nome"]?>">
-                </a>
-                <h3><a href="paginas/listagemhoteis.php?i=<?=$i?>">
-                    <?= $h["nome"]?>
-                    </a>
-                </h3>
-                
-            </div>
-            <?php
-                }
-            ?>-->
-            
-        </div>
         <footer>
         <p>&copy; 2023 Hoteis Sul. Todos os direitos reservados.</p>
     </footer>
