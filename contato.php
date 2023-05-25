56a858b3227de6511692e8aba07b39f9096c4e6e
Maressa <maressabarreto0@gmail.com>
2023-05-25 13:59:14 -0300

    formulário

---
+++ contato.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contato</title>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="contato.css">
+		<link href="imagens/icon.png" rel="icon">
+    </head>
+    <body>
+        <nav class="menu">
+        <div class="Container menu-container">
+                <a href="index.php" class="logo" title="Hotel">
+                 <img src="imagens/logo.png" alt="danilo">
+                </a>
+                <ul class="tt">
+                <li> <a href="index.php" title="menu">Início</a></li>
+                </ul>
+        </div>
+        </nav>
+
+  <main>
+           
+   <section class="postagens">
+       <h3>Hoteis do Sul</h3>
+      
+       <article class="conteudo">
+           
+           <form method="post" action="maressabarreto0@gmail.com">
+           <fieldset>
+               <legend>Entre em contato conosco</legend>
+               
+               <div class="campo">
+                   <label>Nome</label>
+                   <input type="text" name="nome">
+               
+               </div>
+               <div class="campo">
+                   <label>email</label>
+                   <input type="email" name="email">
+               
+               </div>
+               <div class="campo">
+                   <label>mensagens</label>
+                   <textarea name="mensagem"></textarea>
+               
+               </div>
+               <div class="campo">
+                  <button type="submit">Enviar</button>
+               </div>
+   
+               </fieldset>
+           
+           </form>
+ 
+       </article>
+      
+           </section>
+        </main>     
+        
+             <footer>
+             <a href="index.php" class="logo" title="Hotel">
+                <img src="imagens/logo.png" alt="">
+            </a>
+           
+         <address>Malhada de pedras -BA- Rua: Washigton Cardoso dos Santos - Brasil  Tel: (55) 77988726521. </address>
+        </footer>
+</body>
+</html>
+    
