774f1ca84d79d7a41844118ca0bf855a61ca1cde
Elisa-Rizerio-Castro <131362324+Elisa-Rizerio-Castro@users.noreply.github.com>
2023-05-26 10:57:37 -0300

    Update hoteis.php

--- dados/hoteis.php
+++ dados/hoteis.php
@@ -5,48 +5,48 @@ $hoteis = [
         "nome" => "Hotel Fazenda Park",
         "local" => "Florianópolis - Santa Catarina",
         "avaliacao" => "9.5 - Excelente",
-        "preco" => "R$240,00 pernoite."
-         //"foto" => ;
+        "preco" => "R$240,00 pernoite.",
+        "foto" => "../imagens/HotelFazendaPark.jpg",
     ],
 
     [
-        "nome" => "Hotel Isabela Duncan",
+        "nome" => "Guest House Isadora Duncan",
         "local" => "Criciúma - Santa Catarina",
         "avaliacao" => "8.3 - Muito bom",
-        "preco" => "R$103,00 pernoite."
-         //"foto" => ;
+        "preco" => "R$103,00 pernoite.",
+        "foto" => "../imagens/GuestHouseIsadoraDuncan.jpg",
     ],
 
     [
         "nome" => "Hotel Monsieur",
         "local" => "Gravatal - Santa Catarina",
         "avaliacao" => "9.1 - Muito bom",
-        "preco" => "R$112,00 pernoite."
-         //"foto" => ;
+        "preco" => "R$112,00 pernoite.",
+        "foto" => "../imagens/HotelMonsieur.png",
     ],
     /* Rio Grande Do Sul */
     [
         "nome" => "Hotel Kitnet Rififi",
         "local" => "Caxias do Sul - Rio Grande Do Sul",
         "avaliacao" => "8,6 - Muito bom",
-        "preco" => "R$60,00 pernoite."
-         //"foto" => ;
+        "preco" => "R$60,00 pernoite.",
+        "foto" => "../imagens/HotelKitnetRififi.jpg",
     ],
 
     [
         "nome" => "Hotel Petrov",
         "local" => "Porto Alegre - Rio Grande Do Sul",
         "avaliacao" => "7.8 - Bom",
-        "preco" => "R$59,00 pernoite." 
-         //"foto" => ;
+        "preco" => "R$59,00 pernoite.", 
+        "foto" => "../imagens/HotelPetrov.jpg",
     ],
 
       [
         "nome" => "Hotel Finn Kaster Silva",
         "local" => "Três Coroas - Rio Grande Do Sul",
         "avaliacao" => "9.5 - Muito bom",
-        "preco" => "R$114,00 pernoite."
-         //"foto" => 
+        "preco" => "R$114,00 pernoite.",
+        "foto" => "../imagens/HotelFinnKasterSilva.jpg",
         
       ],
       /* Paraná */
@@ -54,24 +54,24 @@ $hoteis = [
         "nome" => "Hotel Evelina",
         "local" => "Curitiba - Paraná",
         "avaliacao" => "9.9 - Excelente",
-        "preco" => "R$280,00 pernoite"
-        //"foto" => 
+        "preco" => "R$280,00 pernoite",
+        "foto" => "../imagens/HotelEvelina.jpg",
       ],
 
       [
         "nome" => "Hotel Triunfo",
         "local" => "Londrina - Paraná",
         "avaliacao" => "8.1 - Bom",
-        "preco" => "R$63,00 pernoite."
-         //"foto" => 
+        "preco" => "R$63,00 pernoite.",
+        "foto" => "../imagens/HotelTriunfo.jpg",
       ],
 
       [
         "nome" => "Hotel Tarobá",
         "local" => "Foz do Iguaçu - Paraná",
         "avaliacao" => "9.8 - Excelente",
-        "preco" => "R$257,00 pernoite."
-         //"foto" => 
+        "preco" => "R$257,00 pernoite.",
+        "foto" => "../imagens/HotelTarobá.jpg",
       ]
 
     ];
