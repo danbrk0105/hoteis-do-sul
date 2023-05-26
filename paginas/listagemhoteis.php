76f7dc740f1a70b165448c4fb6ab7ddbceb7bf38
Elisa-Rizerio-Castro <131362324+Elisa-Rizerio-Castro@users.noreply.github.com>
2023-05-26 10:49:06 -0300

    Update listagemhoteis.php

--- paginas/listagemhoteis.php
+++ paginas/listagemhoteis.php
@@ -1,43 +1,45 @@
+<?php
+  require "../dados/hoteis.php";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Escolha do Estado</title>
-  <link rel="stylesheet" type="text/css" href="../estilo/estilo.css">
+  <link rel="stylesheet" type="text/css" href="../estilo/estilos.css">
  
 </head>
 <body>
   <header class="hEstado">
-    <h1>Escolha o estado que deseja</h1>
+  <img src="../imagens/logo2.png" alt="">
     <nav>
       <ul>
         <li><a href="../index.php">Início</a></li>
         <li><a href="listagemhoteis.php">Hotéis</a></li>
-        <li><a href="contato.php">Contato</a></li>
+        <li><a href="../contato.php">Contato</a></li>
       </ul>
     </nav>
   </header>
-  <section id="banner1">
-    <h2>Bem-vindos ao Hotéis do Sul</h2>
-    <p>Escolha o estado desejado e aproveite nossos preços.</p>
-    <a href="#" class="button">Clique aqui para ver os quartos</a>
-  </section>
+  <section>
+  <div class="container">
+		
+            <?php 
+                foreach ($hoteis as $i =>  $b) {
+                    
+                
+            ?>
 
-  <section id="states">
-    <h2>Escolha um Estado:</h2>
-    <ul>
-      <li><a href="parana.html">Paraná</a></li>
-      <li><a href="riograndedosul.html">Rio Grande do Sul</a></li>
-      <li><a href="santacatarina.html">Santa Catarina</a></li>
-    </ul>
-  </section>
-        <div class="card-hotel">
-            <img src="<?=$h["foto"]?>"alt="<?=$h["nome"]?>">
-            <h3><?=$h["nome"]?></h3>
-            <h4><?=$h["local"]?></h4>
-            <p>Salário: <?=$h["avaliacao"]?></p>
-            <p><?=$h["preco"]?></p>
-            <a href="../index.php" class="link">Voltar</a>
-        </div>
+      <div class="card">
+              <a href="detalhe.php?i=<?=$i?>">
+            <img src="<?=$b["foto"]?>" alt="<?=$b["nome"]?>">
+              </a>
+        <h3><a href="detalhe.php?i=<?=$i?>"> <?=$b["nome"]?></a></h3>
+        
+      </div>
+
+         <?php } ?>
+    </div>
+    </section>
     <footer>
         <p>&copy; 2023 Hoteis Sul. Todos os direitos reservados.</p>
     </footer>
